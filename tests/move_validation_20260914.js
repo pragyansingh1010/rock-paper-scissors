6d1c3ccd0e334d17854f389345f48bcee5bcfaf8
pragyansingh1010 <pragyansingh1010@gmail.com>
2026-09-14 21:37:06 +0530

    test: validate game moves

---
+++ tests/move_validation_20260914.js
@@ -0,0 +1,7 @@
+const moves = ['rock', 'paper', 'scissors'];
+
+console.assert(moves.includes('rock'));
+console.assert(moves.includes('paper'));
+console.assert(moves.includes('scissors'));
+console.assert(!moves.includes('lizard'));
+console.log('Move validation passed');
